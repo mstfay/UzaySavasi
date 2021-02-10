ca696ed7c54f857b98741e3e68911dba1f3ca321
mustafa.ay <aymustafa.32@gmail.com>
2021-02-10 19:25:25 +0300

    Ilk Commitim

---
+++ Assets/Scripts/Learning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 6;
+
+        //if (20<x && x<=30)
+        //{
+        //    Debug.Log("Ebenin Amı!");
+        //}
+        //else if (30<x && x<=99)
+        //{
+        //    Debug.Log("Ananın Amı!");
+        //}
+        //else if (100<x && x != 150)
+        //{
+        //    Debug.Log("Ultra Ananın Amı!");
+        //}
+        //else
+        //{
+        //    Debug.Log("Sıkıntı yooook!");
+        //}
+
+        switch(x)
+        {
+            case 1:
+                Debug.Log("Güzel!");
+                break;
+
+            case 5:
+                Debug.Log("Adamsın!");
+                break;
+
+            default:
+                Debug.Log("Tutmadı be başkan");
+                break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
